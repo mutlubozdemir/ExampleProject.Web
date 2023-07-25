2b36224d0d8a88dcf4481ecfe2da9a85c7ebfa7a
mutlubozdemir <mutlubodemir3@gmail.com>
2023-07-25 13:58:31 +0300

    user and role

---
+++ ExampleProject.Service/AutoMapper/Users/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ExampleProject.Entity.DTOs.Users;
+using ExampleProject.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Service.AutoMapper.Users
+{
+	public class UserProfile:Profile
+	{
+		public UserProfile()
+		{
+			CreateMap<AppUser,UserDto>().ReverseMap();
+		}
+	}
+}
